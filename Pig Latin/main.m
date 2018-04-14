2c357112970466b82992bcc58c46e60bf2c85ec4
Colin Russell <colinrussell101@gmail.com>
2018-04-14 15:08:46 -0400

    Initial Commit

---
+++ Pig Latin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pig Latin
+//
+//  Created by Colin on 2018-04-14.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
